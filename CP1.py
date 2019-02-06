007dc7a128fd8862208a84855bd522a8e941742d
Ross Slater <rossawslater@gmail.com>
2019-02-06 10:40:29 +0100

    Fixed Glauber

--- CP1.py
+++ CP1.py
@@ -2,17 +2,27 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import sys
 
 class IsingModel():
-
-	def __init__(self, N, T, J = 1):
-		self.N = N
-		self.T = T
-		self.J = J
+	#sys argumets N, T , dynamics
+	def __init__(self):#, N, T, random = True, J = 1):
+		self.N = int(sys.argv[1])
+		self.T = float(sys.argv[2])
+		self.J = 1
 		self.plotFigure = plt.figure()
-		self.make_array()
 
-	def make_array(self): #generate NxN matrix of random 1s and 0s
+		if sys.argv[3] == "Random":
+			self.initalize_random()
+
+		if sys.argv[3] == "Uniform":
+			self.initalize_uniform()
+
+	def initalize_uniform(self):
+		self.array = np.ones([self.N, self.N])
+		self.test_array = np.copy(self.array)
+
+	def initalize_random(self):
 		self.array = np.zeros([self.N, self.N])
 		for x in range(0, self.N):
 			for y in range(0, self.N):
@@ -22,12 +32,14 @@ class IsingModel():
 	def get_i(self):
 		i = np.random.randint(0,self.N,(2))
 		self.x = i[0]; self.y = i[1]
+		return self.x, self.y
 
-	def flip_spin(self, x, y):
-		self.test_array[self.x, self.y] = - self.array[self.x, self.y]
+	def flip_spin(self):#, x, y):
+		self.array[self.x, self.y] *= -1
 
-	def get_E(self, x, y):
-		i = self.test_array[x, y]
+	def get_flip_E(self, x, y):
+
+		i = self.array[x, y]
 
 		if x == self.N-1: #boundary conditions so nearest neighbor loops over at far edge
 			x_plus_1 = 0
@@ -42,22 +54,33 @@ class IsingModel():
 		x_minus_1 = x - 1 #if x or y is 0 then -1 indexes to last entry in array so loops over
 		y_minus_1 = y - 1
 
-		self.E = - self.J * i * (self.test_array[x_plus_1, y] + self.test_array[x_minus_1,y]
-				+ self.test_array[x, y_plus_1] + self.test_array[x,y_minus_1])
+		neighbors = np.array([self.array[x_plus_1, y], self.array[x_minus_1,y], self.array[x, y_plus_1], self.array[x,y_minus_1]])
 
-		return self.E
+		nn_sum = np.sum(neighbors)
+
+		E_i = i * nn_sum
+
+		i *= -1
+
+		E_f = i * nn_sum
+
+		return E_i, E_f
 
 	def get_probability(self, E):
+		r = np.random.rand()
+		p = np.exp(-E/self.T)
+
 		if E <= 0:
-			self.array = np.copy(self.test_array)
-		if E > 0:
-			r = np.random.rand()
-			p = np.exp(-E/self.T)
-			print r,p
-			if r <= p:
-				self.array = np.copy(self.test_array)
-			else:
-				pass
+			self.flip_spin()
+		elif r <= p:
+			self.flip_spin()
+		else:
+			pass
+
+	def Glauber(self):
+		x, y = self.get_i()
+		E_i, E_f = self.get_flip_E(x,y)
+		self.get_probability(-E_f+E_i)
 
 
 	def swap_spins(self):
@@ -80,14 +103,6 @@ class IsingModel():
 		self.i = i#self.test_array[self.j_x, self.j_y]
 		self.j = j#self.test_array[self.i_x, self.i_y]
 
-	def Glauber(self):
-		self.get_i()
-		E_1 = self.get_E(self.x, self.y)
-		self.flip_spin(self.x, self.y)
-		E_2 = self.get_E(self.x, self.y)
-		self.get_probability(E_2-E_1)
-		self.test_array = np.copy(self.array)
-
 	def Kawasaki(self):
 		self.get_i()
 		self.swap_spins()
@@ -100,23 +115,23 @@ class IsingModel():
 
 	def updatePlot(self,i):
 		for i in range (2500):
-			self.Kawasaki()
+			# self.Kawasaki()
+			self.Glauber()
 		self.plotFigure.clear()# Clear the old plot
 		plt.imshow(self.array, interpolation = "nearest", cmap = "binary")# Make the new plot
 
 	def Visualise(self):# Function that runs the animaion
-		# self.make_array()
-		ani = animation.FuncAnimation(self.plotFigure, self.updatePlot)#, interval=1 )
+		ani = animation.FuncAnimation(self.plotFigure, self.updatePlot)
 		plt.show()
 
 
 
 # IsingModel(50,1).Visualise()
-x = IsingModel(50,0.01)
+x = IsingModel()#50,2.7)
 # x.make_array()
-for i in range(100000):
-	x.Kawasaki()
-plt.imshow(x.array, interpolation = "nearest", cmap = "binary")
-plt.show()
-
-# x.Visualise()
+# for i in range(100000):
+# 	x.Glauber()
+# plt.imshow(x.array, interpolation = "nearest", cmap = "binary")
+# plt.show()
+#
+x.Visualise()
