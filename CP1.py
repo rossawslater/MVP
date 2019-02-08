25c87ca3035d9dd6704f2efe375392ac0c584d0d
Ross Slater <rossawslater@gmail.com>
2019-02-08 22:16:50 +0100

    Set up skeleton of temperature variation experiment. Need chi function

--- CP1.py
+++ CP1.py
@@ -40,12 +40,13 @@ class IsingModel():
 				self.array[x,y] = np.random.choice([-1,1])
 		self.test_array = np.copy(self.array)
 
-	def get_i(self):
-		i = np.random.randint(0,self.N,(2))
-		self.x = i[0]; self.y = i[1]
-		return self.x, self.y
+	def get_x_y(self):
+		# i = np.random.randint(0,self.N,(2))
+		# self.x = i[0]; self.y = i[1]
+		# return self.x, self.y
+		return np.random.randint(0,self.N), np.random.randint(0,self.N)
 
-	def get_i_and_j(self):  ################## not self? 
+	def get_i_and_j(self):  ################## not self?  # tidy so no using self  shouldn't need, just call get i twoice
 		r = np.random.randint(0,self.N,(4))
 		self.i_x = r[0]; self.i_y = r[1]
 		self.j_x = r[2]; self.j_y = r[3]
@@ -96,25 +97,31 @@ class IsingModel():
 
 	def Glauber(self):
 		# x, y = self.get_i()
-		x,y,_,_ = self.get_i_and_j()
+		# x,y,_,_ = self.get_i_and_j()
+		x,y = self.get_x_y()
 		E_i, E_f = self.get_flip_E(x,y)
 		if self.get_probability(-E_f+E_i) == True:
 			# self.flip_spin(self.x,self.y)
 			self.flip_spin(x,y)
 
 	def Kawasaki(self):
-		self.get_i_and_j()
-		if self.array[self.i_x, self.i_y] != self.array[self.j_x, self.j_y]:
-			E_1_init, E_1_final = self.get_flip_E(self.i_x, self.i_y)
-			E_2_init, E_2_final = self.get_flip_E(self.j_x, self.j_y)
+
+		# self.get_i_and_j()
+		i_x, i_y = self.get_x_y()
+		j_x, j_y = self.get_x_y()
+
+		if self.array[i_x,i_y] != self.array[j_x,j_y]:
+			E_1_init, E_1_final = self.get_flip_E(i_x,i_y)
+			E_2_init, E_2_final = self.get_flip_E(j_x,j_y)
+
 			if self.get_probability(-E_1_final + E_1_init + -E_2_final + E_2_init) == True:
-				self.flip_spin(self.i_x, self.i_y)
-				self.flip_spin(self.j_x, self.j_y)
+				self.flip_spin(i_x,i_y)
+				self.flip_spin(j_x,j_y)
 		else:
 			pass
 
 	def updatePlot(self,i):
-		for i in range (2500):
+		for i in range(self.N**2):
 			self.update()
 		self.plotFigure.clear()# Clear the old plot
 		plt.imshow(self.array, interpolation = "nearest", cmap = "binary")# Make the new plot
@@ -127,20 +134,45 @@ class IsingModel():
 
 class Experiments():
 
-	def __init__(self, N):
+	def __init__(self, N, dynamics):
 		self.N = N
 		self.Magnetisation = []
 		self.Energy = []
+		self.dynamics = dynamics
+		self.init_state = "Uniform"
 
+	def get_M(self,array):
+		self.Magnetisation.append(np.sum(array))
+		# return np.sum(array)
+	def get_chi(self,M_vals,T):
+		chi = 1/(self.N,T)
 	def vary_T(self, min = 1, max = 3):
-		T_vals = np.linspace(min,max,0.1)
-		# self.IsingModel()
+		# T_vals = np.linspace(min,max,(max-min)/0.1)
+		T_vals = [2.7]
 
 		for T in T_vals:
-			sim = IsingModel(self.N, self.T)
-			for i in range(self.N*100):
-				pass
 
+			sim = IsingModel(self.N, T, self.dynamics, self.init_state)
+
+			for sweep in range(100): #equlibriate
+				for i in range(self.N**2):
+					sim.update()
+				print "Equlibrium sweep", sweep
+			#equlibriated
+
+			for sweep in range(10000): #equlibriate
+				for i in range(self.N**2):
+					sim.update()
+				if sweep%10 == 0:
+					self.get_M(sim.array)
+				print sweep
+			#finished 1 T_val sample
+
+		print self.Magnetisation
+		print len(self.Magnetisation)
+		print np.mean(self.Magnetisation)
+				# else:
+				# 	print "Pass"
 
 def main():
 	N = int(sys.argv[1])
@@ -148,6 +180,7 @@ def main():
 	dynamics = sys.argv[3]
 	init_state = sys.argv[4]
 
-	IsingModel(N,T,dynamics,init_state).Visualise()
-
+	# IsingModel(N,T,dynamics,init_state).Visualise()
+	x = Experiments(N,dynamics)
+	x.vary_T()
 main()
