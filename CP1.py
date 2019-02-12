94bff3ceb8f47616a397ced3bf74214a8051163a
Ross Slater <rossawslater@gmail.com>
2019-02-12 21:49:07 +0000

    semi final

--- CP1.py
+++ CP1.py
@@ -7,10 +7,11 @@ import datetime
 
 class IsingModel():
 
-	def __init__(self, N, T, dynamics, init_state, J = 1):
-		self.N = N
-		self.T = T
+	def __init__(self, N, T, dynamics, init_state, J = 1, Kb = 1):
+		self.N = int(N)
+		self.T = float(T)
 		self.J = J
+		self.Kb = Kb
 		self.plotFigure = plt.figure() #required for animation
 
 		if dynamics == "Glauber":
@@ -28,10 +29,6 @@ class IsingModel():
 		else:
 			print "Input Error"
 			exit()
-
-		# if type(N) or type(T) != float():
-		# 	print "Input Error"
-		# 	exit()
 
 	def initalize_uniform(self, dynamics):
 		self.array = np.ones([self.N, self.N])
@@ -51,6 +48,13 @@ class IsingModel():
 
 	def flip_spin(self, x, y):
 		self.array[x,y] *= -1
+
+	def check_nn(self,i_x,i_y,j_x,j_y):
+		if abs(i_x-j_x) == 1 or abs(i_y-j_y) == 1 or (i_x-j_x)%self.N-1 == 0 or (i_y-j_y)%self.N-1 == 0:
+			return True
+
+		else:
+			return False
 
 	def get_dE(self, x, y):
 
@@ -73,15 +77,15 @@ class IsingModel():
 
 		nn_sum = np.sum(neighbors)
 
-		E_i = i * nn_sum
+		E_i = i * nn_sum * self.J
 
-		E_f = - i * nn_sum #flip spin of i
+		E_f = - i * nn_sum * self.J #flip spin of i
 
 		return -E_f + E_i
 
 	def get_probability(self, E):
 		r = np.random.rand()
-		p = np.exp(-E/self.T)
+		p = np.exp(-E/(self.Kb * self.T))
 
 		if E <= 0:
 			return True
@@ -104,15 +108,19 @@ class IsingModel():
 		if self.array[i_x,i_y] != self.array[j_x,j_y]:
 			dE_1 = self.get_dE(i_x,i_y)
 			dE_2 = self.get_dE(j_x,j_y)
-			if self.get_probability(dE_1 + dE_2) == True:
-				self.flip_spin(i_x,i_y)
-				self.flip_spin(j_x,j_y)
-		else:
-			pass
+			if self.check_nn(i_x,i_y,j_x,j_y) == True:
+				if self.get_probability(dE_1 + dE_2 + 4) == True:
+						self.flip_spin(i_x,i_y)
+						self.flip_spin(j_x,j_y)
+			else:
+				if self.get_probability(dE_1 + dE_2) == True:
+						self.flip_spin(i_x,i_y)
+						self.flip_spin(j_x,j_y)
 
 	def updatePlot(self,i): #move to plotting class?
-		for i in range(self.N**2):
-			self.update()
+		for sweep in range(10):
+			for i in range(self.N**2):
+				self.update()
 		self.plotFigure.clear()# Clear the old plot
 		plt.imshow(self.array, interpolation = "nearest", cmap = "binary")# Make the new plot
 		plt.axis('off')
@@ -121,23 +129,6 @@ class IsingModel():
 		ani = animation.FuncAnimation(self.plotFigure, self.updatePlot)
 		plt.show()
 
-	# def get_E_no_DC(self, x, y): #move to Experiments class
-	# 	i = self.array[x, y]
-	#
-	# 	if x == self.N-1: #boundary conditions so nearest neighbor loops over at far edge
-	# 		x_plus_1 = 0
-	# 	else:
-	# 		x_plus_1 = x + 1
-	#
-	# 	if y == self.N-1:
-	# 		y_plus_1 = 0
-	# 	else:
-	# 		y_plus_1 = y + 1
-	#
-	# 	E = i * np.sum(np.array([self.array[x_plus_1, y], self.array[x, y_plus_1]])) #spin of i * sum of neaest neighbor spins
-	#
-	# 	return E
-
 class Experiments():
 
 	def __init__(self, N, dynamics, min = 1, max = 3, step = 0.1):
@@ -145,9 +136,10 @@ class Experiments():
 		self.T_vals = np.arange(min,max,step)
 		self.dynamics = dynamics
 		self.init_state = "Uniform" #just define in instance of creating object?
+		# self.plotFigure = plt.figure()
 
 	def get_M(self,array):
-		return np.sum(array)
+		return abs(np.sum(array))
 
 	def get_Chi(self,M_Vals,T):
 		return (np.mean(M_Vals**2) - np.mean(M_Vals)**2)/(self.N * T)
@@ -156,6 +148,7 @@ class Experiments():
 		E = 0
 		for x in range(self.N):
 			for y in range(self.N):
+
 				s = array[x, y]
 
 				if x == self.N-1: #boundary conditions so nearest neighbor loops over at far edge
@@ -230,10 +223,9 @@ class Experiments():
 
 		for i in range(no_steps):
 			for k in range(no_new_measurements): #define what this should be, 100?
-				for j in range(no_measurements):
-
-					new_samples[i] = np.random.choice(Data[i], size = len(Data[0]))
-				new_measurements[i,k] = func(new_samples[i], self.T_vals[i])
+				# for j in range(no_measurements):
+				new_samples[i] = np.random.choice(Data[i], size = len(Data[0]))
+			new_measurements[i,k] = func(new_samples[i], self.T_vals[i])
 
 			stdevs[i] = np.sqrt(np.mean(new_measurements[i]**2) - np.mean(new_measurements[i])**2)
 
@@ -242,7 +234,7 @@ class Experiments():
 	def get_mean_vals(self):
 		mean_M = []
 		for i in range(len(self.Magnetisation)):
-			mean_M.append(abs(np.mean(self.Magnetisation[i]))) #absolute value of mean of magnetisation
+			mean_M.append(np.mean(self.Magnetisation[i])) #absolute value of mean of magnetisation
 
 		mean_E = []
 		for i in range(len(self.Energy)):
@@ -257,8 +249,20 @@ class Experiments():
 
 		return mean_vals
 
-	def output(self):
-		pass
+
+	# def updatePlot(self,i): #move to plotting class?
+	# 	# for sweep in range(10):
+	# 	# 	for i in range(self.N**2):
+	# 	# 		self.update()
+	# 	self.plotFigure.clear()# Clear the old plot
+	# 	plt.imshow(self.sim.array, interpolation = "nearest", cmap = "binary")# Make the new plot
+	# 	plt.axis('off')
+	#
+	# def Visualise(self):# Function that runs the animaion
+	# 	# self.sim = IsingModel(self.N, T, self.dynamics, "Uniform")
+	# 	ani = animation.FuncAnimation(self.plotFigure, self.updatePlot)
+	# 	plt.show()
+
 
 def main():
 	N = int(sys.argv[1])
@@ -266,17 +270,20 @@ def main():
 	dynamics = sys.argv[3]
 	init_state = sys.argv[4]
 
-	# IsingModel(N,T,dynamics,init_state).Visualise()
-	Energy, Magnetisation, C, Chi =  Experiments(N,dynamics).vary_T()
+	IsingModel(N,T,dynamics,init_state).Visualise()
+	exp = Experiments(N,dynamics)
+	# exp.Visualise()
+	Energy, Magnetisation, C, Chi =  exp.vary_T()
+
 	#
-	# date_time = str(datetime.datetime.now())
+	date_time = str(datetime.datetime.now())
 	# np.savetxt(str("Data/Energy " + date_time +  ".txt"), Energy)
 	# np.savetxt(str("Data/Magnetisation " + date_time +  ".txt"), Magnetisation)
 	# np.savetxt(str("Data/Suscepibility " + date_time +  ".txt"), Chi)
 	# np.savetxt(str("Data/Heat Capacity " + date_time +  ".txt"), C)
 	#
 	#
-	# np.savetxt("Data/C_Error" + date_time +  ".txt", self.get_error(Energy, x.get_C))
-	# np.savetxt("Data/Chi_Error" + date_time +  ".txt", self.get_error(Magnetisation, x.get_Chi))
+	np.savetxt("Data/C_Error" + date_time +  ".txt", exp.get_error(Energy, exp.get_C))
+	np.savetxt("Data/Chi_Error" + date_time +  ".txt", exp.get_error(Magnetisation, exp.get_Chi))
 
 main()
